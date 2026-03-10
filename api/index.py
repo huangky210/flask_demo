ae01551be9174bc26aa1a1f0e1c3b1288a32d28e
huangky <huangkunyao@huangkydeMacBook-Air.local>
2026-03-10 11:07:55 +0800

    demo演示

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({
+        "status": "success",
+        "message": "来自 Flask 后端的问候！",
+        "data": "这是通过 Vercel Serverless Function 返回的数据。"
+    })
+
+# 必须导出 app 对象供 Vercel 调用
+if __name__ == '__main__':
+    app.run()
